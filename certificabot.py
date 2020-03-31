f0db23f954496c3cbf2aaa6e90fb256fc3e8d117
Gabriele <sucre.91@hotmail.it>
2020-03-31 10:09:31 +0200

    New form update

--- certificabot.py
+++ certificabot.py
@@ -4,7 +4,7 @@
 from utils import *
 import os
     
-TOKEN = ''
+TOKEN = 
 
 bot = telepot.Bot(TOKEN)
 
@@ -34,16 +34,21 @@ def on_chat_message(msg):
 
   elif state_cache[telegram_name] == "certificabot.name":
     data_cache[telegram_name].name = msg['text']
-    bot.sendMessage(chat_id, "Dove e quando sei nato (Data e Luogo)?")
+    bot.sendMessage(chat_id, "Dove e quando sei nato (Data, Luogo e sigla della Provincia)?")
     state_cache[telegram_name] = "certificabot.birth"
 
   elif state_cache[telegram_name] == "certificabot.birth":
     data_cache[telegram_name].birth = msg['text']
-    bot.sendMessage(chat_id, "Dove risiedi (indirizzo completo)?")
+    bot.sendMessage(chat_id, "Dove risiedi (città, sigla della Provincia e via)?")
     state_cache[telegram_name] = "certificabot.address"
 
   elif state_cache[telegram_name] == "certificabot.address":
     data_cache[telegram_name].address = msg['text']
+    bot.sendMessage(chat_id, "Qual è il tuo domicilio (città, sigla della Provincia e via)?")
+    state_cache[telegram_name] = "certificabot.address2"
+
+  elif state_cache[telegram_name] == "certificabot.address2":
+    data_cache[telegram_name].address2 = msg['text']
     bot.sendMessage(chat_id, "Qual è il tuo numero di telefono?")
     state_cache[telegram_name] = "certificabot.phone"
 
@@ -54,12 +59,37 @@ def on_chat_message(msg):
 
   elif state_cache[telegram_name] == "certificabot.document":
     data_cache[telegram_name].document = "Carta d'Indentità n."+str(msg['text'])
-    bot.sendMessage(chat_id, "Per quale motivo devi uscire?")
+    bot.sendMessage(chat_id, "Da chi è stato rilasciato?")
+    state_cache[telegram_name] = "certificabot.docuauth"
+
+  elif state_cache[telegram_name] == "certificabot.docuauth":
+    data_cache[telegram_name].documentAuth = str(msg['text'])
+    bot.sendMessage(chat_id, "Da che regione parti?")
+    state_cache[telegram_name] = "certificabot.regionstart"
+
+  elif state_cache[telegram_name] == "certificabot.regionstart":
+    data_cache[telegram_name].startingRegion = str(msg['text'])
+    bot.sendMessage(chat_id, "Da che indirizzo parti?")
+    state_cache[telegram_name] = "certificabot.addressstart"
+
+  elif state_cache[telegram_name] == "certificabot.addressstart":
+    data_cache[telegram_name].startingAddress = str(msg['text'])
+    bot.sendMessage(chat_id, "In che regione vai?")
+    state_cache[telegram_name] = "certificabot.regionstop"
+
+  elif state_cache[telegram_name] == "certificabot.regionstop":
+    data_cache[telegram_name].destinationRegion = str(msg['text'])
+    bot.sendMessage(chat_id, "A che indirizzo arrivi?")
+    state_cache[telegram_name] = "certificabot.addressstop"
+
+  elif state_cache[telegram_name] == "certificabot.addressstop":
+    data_cache[telegram_name].destinationAddress = str(msg['text'])
+    bot.sendMessage(chat_id, "Per quale motivo ti stai spostando?")
     state_cache[telegram_name] = "certificabot.motivation"
 
   elif state_cache[telegram_name] == "certificabot.motivation":
     data_cache[telegram_name].motivation = msg['text']
-    bot.sendMessage(chat_id, "Scrivi \"1\" se per comprovate esigenze lavorative;\nScrivi \"2\" se per situazioni di necessità;\nScrivi \"3\" motivi di salute;\nScrivi \"4\" se per rientro presso il proprio domicilio, abitazione o residenza")
+    bot.sendMessage(chat_id, "Scrivi \"1\" se per comprovate esigenze lavorative;\nScrivi \"2\" assoluta urgenza (\"per trasferimenti in comune diverso\", come previsto dall’art. 1, comma 1, lettera b) del Decreto del Presidente del Consiglio dei Ministri 22 marzo 2020);\nScrivi \"3\" situazione di necessità (per spostamenti all’interno dello stesso comune o che rivestono carattere di quotidianità o che, comunque, siano effettuati abitualmente in ragione della brevità delle distanze da percorrere);\nScrivi \"4\" se per motivi di salute.")
     state_cache[telegram_name] = "certificabot.reason"
 
   elif state_cache[telegram_name] == "certificabot.reason":
@@ -78,7 +108,7 @@ def on_chat_message(msg):
     filename = "Autocertificazione_{}".format(data_cache[telegram_name].name)
     mdFile = mdutils.MdUtils(file_name=filename)
     data_cache[telegram_name].printPerson(mdFile)
-    printNotice(mdFile)
+    printNotice(mdFile, data_cache[telegram_name])
     printReason(data_cache[telegram_name].reason,mdFile)
     printMotivation(data_cache[telegram_name].motivation, mdFile)
     printFooter(mdFile)
--- utils.py
+++ utils.py
@@ -4,37 +4,44 @@ import telepot
 import mdutils.mdutils
 import markdown2pdf3
 
-reasons = ["comprovate esigenze lavorative.","situazioni di necessità.","motivi di salute.","rientro presso il proprio domicilio, abitazione o residenza."]
+reasons = ["comprovate esigenze lavorative.","assoluta urgenza (\"per trasferimenti in comune diverso\", come previsto dall’art. 1, comma 1, lettera b) del Decreto del Presidente del Consiglio dei Ministri 22 marzo 2020).","situazione di necessità (per spostamenti all’interno dello stesso comune o che rivestono carattere di quotidianità o che, comunque, siano effettuati abitualmente in ragione della brevità delle distanze da percorrere).","motivi di salute."]
 
 class person:
-  def __init__(self, name="", birth="", address="", document="", phone=""):
+  def __init__(self, name="", birth="", address="", document="", phone="", documentAuth=""):
     self.name = name
     self.birth = birth
     self.address = address
+    self.address2 = address
     self.document = document
+    self.documentAuth = documentAuth
     self.phone = phone
     self.reason = -1
     self.motivation = ""
+    self.startingRegion = ""
+    self.startingAddress = ""
+    self.destinationRegion = ""
+    self.destinationAddress = ""
 
   def printPerson(self, mdfile):
     mdfile.new_line("")
     mdfile.new_line("")
-    mdfile.new_header(level=3, title="**AUTODICHIARAZIONE DI SPOSTAMENTO**")
-    mdfile.new_paragraph("Il sottoscritto **{}**, residente in **".format(self.name.capitalize())+self.address+"**, nato a **"+self.birth+"**, identificabile tramite **"+self.document+"** e con utenza telefonica **{}**.".format(str(self.phone)))
-    mdfile.new_line("")
+    mdfile.new_header(level=3, title="**AUTODICHIARAZIONE AI SENSI DEGLI ARTT. 46 E 47 D.P.R. N. 445/2000**")
+    mdfile.new_line("Il sottoscritto **{}**,".format(self.name.capitalize())+"nato il **"+self.birth+"**,"+"residente in **"+self.address+"**,"+"domiciliato in **"+self.address2+"**,"+"identificato a mezzo **"+self.document+"**,"+"rilasciato da **"+self.documentAuth+"**,"+"e con utenza telefonica **{}**.".format(str(self.phone)))
     mdfile.new_line("Consapevole delle conseguenze penali previste in caso di dichiarazioni mendaci a pubblico ufficiale **(art 495 c.p.)**")
 
   def showPerson(self):
     return "name: {}\nbirth: {}\naddress: {}\ndocument: {}\nphone: {}\nreason: {}\nmotivation: {}".format(self.name, self.birth, self.address, self.document, self.phone, reasons[self.reason], self.motivation)
 
 
-def printNotice(mdfile):
+def printNotice(mdfile, person):
   mdfile.new_line("")
   mdfile.new_line("")
   mdfile.new_header(level=4, title="**DICHIARA SOTTO LA PROPRIA RESPONSABILITÀ**")
-  mdfile.new_paragraph("* Di essere a **conoscenza delle misure di contenimento del contagio** di cui al combinato disposto dell'**art. 1 del Decreto del _Presidente del Consiglio dei Ministri_ 8 marzo 2020 e dell'art. 1, comma 1, del Decreto del _Presidente del Consiglio dei Ministri_ del 9 marzo 2020** concernenti **lo spostamento delle persone fisiche all'interno di tutto il territorio nazionale**;")
-  mdfile.new_paragraph("* **Di non essere sottoposto alla misura della quarantena** e di non essere risultato positivo al virus COVID-19 di cui all'**articolo 1, comma 1, lettera c), del Decreto del _Presidente del Consiglio dei Ministri_ dell' 8 marzo 2020**;")
-  mdfile.new_paragraph("* **Di essere a conoscenza delle sanzioni previste, dal combinato disposto dell'art. 3, comma 4, del D.L. 23 febbraio 2020, n. 6 e dell'art. 4, comma 1**, del Decreto del _Presidente del Consiglio dei Ministri_ dell' 8 marzo 2020 **in caso di inottemperanza delle predette misure di contenimento** (art. 650 c.p. salvo che il fatto non costituisca più grave reato);")
+  mdfile.new_paragraph("* **di non essere sottoposto alla misura della quarantena ovvero di non essere risultato positivo al COVID-19** __(fatti salvi gli spostamenti disposti dalle Autorità sanitarie)__;")
+  mdfile.new_paragraph("* che lo spostamento è iniziato da **{}** con destinazione **{}**;".format(person.startingAddress, person.destinationAddress))
+  mdfile.new_paragraph("* **di essere a conoscenza delle misure di contenimento del contagio vigenti alla data odierna ed adottate ai sensi degli artt. 1 e 2 del decreto legge 25 marzo 2020, n. 19, concernenti le limitazioni alle possibilità di spostamento delle persone fisiche all’interno di tutto il territorio nazionale**;")
+  mdfile.new_paragraph("* **di essere a conoscenza delle ulteriori limitazioni disposte con provvedimenti del Presidente della Regione {} e del Presidente della Regione {} e che lo spostamento rientra in uno dei casi consentiti dai medesimi provvedimenti: {}**;".format(person.startingRegion, person.destinationRegion, person.motivation))
+  mdfile.new_paragraph("* **di essere a conoscenza delle sanzioni previste dall’art. 4 del decreto legge 25 marzo 2020, n. 19**;")
 
 def printReason(index, mdfile):
   mdfile.new_line("* Che lo spostamento è determinato da: **{}**".format(reasons[index]))
